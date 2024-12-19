06a535ec104969af3d1d9a6becb3b7b7a7a42156
AmanKuamr <amankumar17j@gmail.com>
2024-12-19 20:47:46 +0530

    third commit

---
+++ 1792. Maximum Average Pass Ratio.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define d double 
+
+class Solution {
+public:
+    double gain(d a,d b){
+        return ((a+1)/(b+1))-(a/b);
+    }
+    double maxAverageRatio(vector<vector<int>>& cls, int es) {
+
+        priority_queue<tuple<d,d,d>> pq;
+
+        for(auto i:cls){
+            pq.push({gain(double(i[0]),double(i[1])),double(i[0]),double(i[1])});
+        }
+
+        while(es){
+            auto[_,a,b]=pq.top();
+            pq.pop();
+            a++;
+            b++;
+            pq.push({gain(a,b),a,b});
+            es--;
+        }
+
+        double avgsum=0.0;
+        while(!pq.empty()){
+            auto [_,a,b]=pq.top();
+            pq.pop();
+            double avg=a/b;
+            avgsum+=avg;
+        }
+        double ans=avgsum/(double)cls.size();
+
+        return ans;
+    }
+};
